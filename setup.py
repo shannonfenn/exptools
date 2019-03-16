21daa1c3b4c401856db5281aeda58820dc12b18e
Shannon Fenn <shannon.fenn@gmail.com>
2019-03-16 19:23:09 +0900

    added bundle script

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
             'list_subdirs = scripts.list_subdirs:main',
             'summarise_multiexp = scripts.summarise_multiexp:main',
             'submit_bundled_jobs = scripts.submit_bundled_jobs:main',
+            'bundle = scripts.bundle:main',
         ],
     }
     # url='https://github.com/shannonfenn/data-tools',
